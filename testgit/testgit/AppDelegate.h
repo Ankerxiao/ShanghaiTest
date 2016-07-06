1e1d2dffc6a94fe01eac8c07739dc691928e2663
Ankerxiao <807132689@qq.com>
2016-07-06 10:31:12 +0800

    test

---
+++ testgit/testgit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testgit
+//
+//  Created by Anker Xiao on 16/7/6.
+//  Copyright © 2016年 AnkerXiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testgit/testgit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testgit
+//
+//  Created by Anker Xiao on 16/7/6.
+//  Copyright © 2016年 AnkerXiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
